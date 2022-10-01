d934b9777da7fcfe645f399e7ed092af7566e915
M Haidar Hanif <git@mhaidarhanif.com>
2022-10-01 23:52:35 +0700

    change local to X11

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo cp libmlx.a /usr/local/lib
-sudo cp libmlx_Darwin.a /usr/local/lib
-sudo cp mlx.h /usr/local/include
+sudo cp libmlx.a /usr/X11/lib
+sudo cp libmlx_Darwin.a /usr/X11/lib
+sudo cp mlx.h /usr/X11/include
 
